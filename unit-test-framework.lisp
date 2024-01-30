9a20833d3f384907ae9ffca9baafe3b2e4098831
Lousher <779546225@qq.com>
2024-01-30 21:25:53 +0800

    a simple test framework

--- unit-test-framework.lisp
+++ unit-test-framework.lisp
@@ -10,12 +10,48 @@
   (report-result (= (+ -1 -3) -4) '(= (+ -1 -3) -4)))
 
 (defun report-result (result form)
-  (format t "~:[FAIL~;pass~] ... ~a~%" result form))
+  (format t "~:[FAIL~;pass~] ... ~a; ~a~%" result *test-name* form)
+  result)
 
-(defmacro check (form)
-  `(report-result ,form ',form))
+;(defmacro check (form)
+; `(report-result ,form ',form))
 
-(defun test-* ()
-  (check (= (* 1 2) 2))
-  (check (= (* 2 3) 6))
-  (check (= (* 3 4) 12)))
+;(defun test-* ()
+;  (check (= (* 1 2) 2))
+;  (check (= (* 2 3) 6))
+;  (check (= (* 3 4) 12)))
+
+(defmacro with-gensyms ((&rest names) &body body)
+  `(let ,(loop for n in names collect `(,n (gensym)))
+     ,@body))
+
+(defmacro combine-results (&body forms)
+  (with-gensyms (result)
+    `(let ((,result t))
+       ,@(loop for f in forms collect `(unless ,f (setf ,result nil)))
+       ,result)))
+
+
+(defmacro check (&body forms)
+  `(combine-results
+     ,@(loop for f in forms collect `(report-result ,f ',f))))
+
+(deftest test-* ()
+  (check
+    (= (* 2 2) 4)
+    (= (* 3 3) 9)))
+
+(deftest test-arithemetic ()
+  (combine-results
+    (test-+)
+    (test-*)))
+
+(defvar *test-name* nil)
+
+(defmacro deftest (name parameters &body body)
+  `(defun ,name ,parameters
+     (let ((*test-name* (append *test-name* (list ',name)))) ,@body)))
+
+(deftest test-+-new ()
+  (check
+    (= (+ 1 1) 2)))
