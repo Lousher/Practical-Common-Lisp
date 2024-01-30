cac946b92da9a91056eadb25768d01b1a1a421b9
Lousher <779546225@qq.com>
2024-01-30 13:16:01 +0800

    a simple macro check for a single test label

--- unit-test-framework.lisp
+++ unit-test-framework.lisp
@@ -5,9 +5,17 @@
    (= (+ -1 -3) -4)))
 
 (defun test-+with-format ()
-  (format t "~:[FAIL~;pass~] ... ~a~%" (= (+ 1 2) 3) '(= (+ 1 2) 3))
-  (format t "~:[FAIL~;pass~] ... ~a~%" (= (+ 1 2 3) 6) '(= (+ 1 2 3) 6))
-  (format t "~:[FAIL~;pass~] ... ~a~%" (= (+ -1 -3) -4) '(= (+ -1 -3) -4)))
+  (report-result  (= (+ 1 2) 3) '(= (+ 1 2) 3))
+  (report-result (= (+ 1 2 3) 6) '(= (+ 1 2 3) 6))
+  (report-result (= (+ -1 -3) -4) '(= (+ -1 -3) -4)))
 
 (defun report-result (result form)
   (format t "~:[FAIL~;pass~] ... ~a~%" result form))
+
+(defmacro check (form)
+  `(report-result ,form ',form))
+
+(defun test-* ()
+  (check (= (* 1 2) 2))
+  (check (= (* 2 3) 6))
+  (check (= (* 3 4) 12)))
